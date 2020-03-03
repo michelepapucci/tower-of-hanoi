bc5224385b2a7e9b073c557efa577a926ccaaa3e
michelepapucci <michele.papucci97@gmail.com>
2020-03-04 00:35:35 +0100

    inserted-first-ring-into-stick

--- js/hanoi.js
+++ js/hanoi.js
@@ -1,6 +1,5 @@
 $(function () {
-    $(window).on('resize', function(){
-        console.log("kek");
+    $(window).on('resize', function () {
         camera.aspect = renderer.domElement.clientWidth / renderer.domElement.clientHeight;
         camera.updateProjectionMatrix();
         renderer.setSize(renderer.domElement.clientWidth, renderer.domElement.clientHeight);
@@ -9,11 +8,14 @@ $(function () {
     let scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     let renderer = new THREE.WebGLRenderer();
+    let n_sticks = 3;
+    let stick_d = 0.25;
+    let board_width = 6;
+    let board_padding = 1;
 
-    //TODO: Structure dimension based off window length and width
     function createHanoiStructure() {
         let material;
-        let board_geom = new THREE.BoxGeometry(6, 0.5, 1);
+        let board_geom = new THREE.BoxGeometry(board_width, 0.5, 1);
         let loader = new THREE.TextureLoader();
         loader.crossOrigin = "";
         loader.load('./texture/wood.jpg',
@@ -24,43 +26,40 @@ $(function () {
                 let board = new THREE.Mesh(board_geom, material);
                 board.castShadow = true;
                 board.receiveShadow = true;
-
-                let stick_geom = new THREE.CylinderGeometry(0.25, 0.25, 3, 20);
-                let first_stick = new THREE.Mesh(stick_geom, material);
-                first_stick.castShadow = true;
-                first_stick.receiveShadow = true;
-                first_stick.position.set(-2, 1.5, 0.25);
-
-                let second_stick = new THREE.Mesh(stick_geom, material);
-                second_stick.castShadow = true;
-                second_stick.receiveShadow = true;
-                second_stick.position.set(0, 1.5, 0.25);
-
-                let third_stick = new THREE.Mesh(stick_geom, material);
-                third_stick.castShadow = true;
-                third_stick.receiveShadow = true;
-                third_stick.position.set(2  , 1.5, 0.25);
-
                 scene.add(board);
-                scene.add(first_stick);
-                scene.add(second_stick);
-                scene.add(third_stick);
+
+                //Good solution but only works for odd number of sticks. Good for now.
+                let stick_geom = new THREE.CylinderGeometry(stick_d, stick_d, 3, 20);
+                let stick_x = (board_width - board_padding) / n_sticks;
+                for(let i = - Math.floor(n_sticks/2); i < n_sticks - Math.floor(n_sticks/2); i++) {
+                    let x = stick_x * i;
+                    let stick = new THREE.Mesh(stick_geom, material);
+                    stick.castShadow = true;
+                    stick.receiveShadow = true;
+                    stick.position.set(x, 1.5, 0.25);
+                    scene.add(stick);
+                }
             },
+
             function () {
             },
+
             function (error) {
                 console.log(error);
             });
     }
 
-    function createRing() {
+    function createRing(stick_position) {
         let material = new THREE.MeshBasicMaterial({
             color: 0x00ff00
         });
-        var geometry = new THREE.RingGeometry(0.25, 0.5, 20);
+
+        var geometry = new THREE.TorusGeometry(0.5, 0.1, 8, 20);
         var mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(-4, 0, 0);
+        //Half height of board which is set to 0,0 + thickness of ring
+        mesh.position.set(0, 0.25 + 0.1, 0.25);
         scene.add(mesh);
+        mesh.rotation.x = Math.PI / 2;
     }
 
     function animate() {
@@ -73,17 +72,20 @@ $(function () {
         document.body.appendChild(renderer.domElement);
 
         createHanoiStructure();
-        createRing();
+        ring = createRing();
 
         camera.position.z = 5;
         camera.position.y = 1;
-        const light = new THREE.PointLight(0xffffff, 5, 100);
+
+        let light = new THREE.PointLight(0xffffff, 5, 100);
         light.position.set(0, 0, 10);
         scene.add(light);
 
+        renderer.render(scene, camera);
         animate();
     }
 
     setup();
+
 });
 	
